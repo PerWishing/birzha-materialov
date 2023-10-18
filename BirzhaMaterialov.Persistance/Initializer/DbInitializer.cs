a9568de387d0ad9834ccc7cf8b3aa8b713511546
PerWishing <holden201705@gmail.com>
2023-10-18 18:37:53 +0300

    added DbInitializer

---
+++ BirzhaMaterialov.Persistance/Initializer/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BirzhaMaterialov.Persistance.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public DbInitializer(IServiceScopeFactory scopeFactory)
+        {
+            this._scopeFactory = scopeFactory;
+        }
+
+        public void Initialize()
+        {
+            using (var serviceScope = _scopeFactory.CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>())
+                {
+                    if (context != null)
+                    {
+                        context.Database.Migrate();
+                    }
+                }
+            }
+        }
+    }
+}
