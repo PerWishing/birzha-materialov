1923e1dd6bbe1629994ee337e860b5d5e9adb06d
PerWishing <holden201705@gmail.com>
2023-10-18 19:22:24 +0300

    added CreateSeller command

---
+++ BirzhaMaterialov.Application/Sellers/Commands/CreateSeller/CreateSellerCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Sellers.Commands.CreateSeller
+{
+    public class CreateSellerCommand : IRequest<int>
+    {
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ BirzhaMaterialov.Application/Sellers/Commands/CreateSeller/CreateSellerCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using BirzhaMaterialov.Application.Interfaces;
+using BirzhaMaterialov.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Sellers.Commands.CreateSeller
+{
+    public class CreateSellerCommandHandler : IRequestHandler<CreateSellerCommand, int>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public CreateSellerCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> Handle(CreateSellerCommand request,
+    CancellationToken cancellationToken)
+        {
+            var seller = new Seller
+            {
+                Name = request.Name,
+            };
+
+            await _dbContext.Sellers.AddAsync(seller);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return seller.Id;
+        }
+    }
+}
