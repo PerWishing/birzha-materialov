75199864932214da97668dfb8d62539622867e9f
PerWishing <holden201705@gmail.com>
2023-10-18 16:46:14 +0300

    added ApplicationDbContext class

---
+++ BirzhaMaterialov.Persistance/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BirzhaMaterialov.Application.Interfaces;
+using BirzhaMaterialov.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Persistance
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Seller> Sellers { get; set;}
+        public DbSet<Material> Materials { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
