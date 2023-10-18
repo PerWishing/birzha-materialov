678ce61895d9e70d4e764e903f8ab942586528e1
PerWishing <holden201705@gmail.com>
2023-10-18 16:34:21 +0300

    added IApplicationDbContext interface to Application

---
+++ BirzhaMaterialov.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BirzhaMaterialov.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BirzhaMaterialov.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Seller> Sellers { get; }
+
+        DbSet<Material> Materials { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
