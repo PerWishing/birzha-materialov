8e2c59fffe8d28d8a14d23fb6219b72f27f1fe73
PerWishing <holden201705@gmail.com>
2023-10-19 18:15:53 +0300

    added UpdateMaterialCommand

---
+++ BirzhaMaterialov.Application/Materials/Commands/UpdateMaterial/UpdateMaterialCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Materials.Commands.UpdateMaterial
+{
+    public class UpdateMaterialCommand: IRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+}
---
+++ BirzhaMaterialov.Application/Materials/Commands/UpdateMaterial/UpdateMaterialCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using BirzhaMaterialov.Application.Common.Exceptions;
+using BirzhaMaterialov.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Materials.Commands.UpdateMaterial
+{
+    public class UpdateMaterialCommandHandler : IRequestHandler<UpdateMaterialCommand>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public UpdateMaterialCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Handle(UpdateMaterialCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Materials.FirstOrDefaultAsync(material => material.Id == request.Id,
+                cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(entity), request.Id);
+            }
+
+            entity.Name = request.Name;
+            entity.Price = request.Price;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
