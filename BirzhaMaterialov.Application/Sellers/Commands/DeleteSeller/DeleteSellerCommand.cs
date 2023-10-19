fc93ec800b2cb10753174185beb35fd19e26a79c
PerWishing <holden201705@gmail.com>
2023-10-19 13:49:21 +0300

    added DeleteSellerCommand

---
+++ BirzhaMaterialov.Application/Sellers/Commands/DeleteSeller/DeleteSellerCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Sellers.Commands.DeleteSeller
+{
+    public class DeleteSellerCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ BirzhaMaterialov.Application/Sellers/Commands/DeleteSeller/DeleteSellerCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using BirzhaMaterialov.Application.Common.Exceptions;
+using BirzhaMaterialov.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Sellers.Commands.DeleteSeller
+{
+    public class DeleteSellerCommandHandler : IRequestHandler<DeleteSellerCommand>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public DeleteSellerCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Handle(DeleteSellerCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Sellers.FirstOrDefaultAsync(seller => seller.Id == request.Id,
+                cancellationToken);
+
+            if(entity == null)
+            {
+                throw new NotFoundException(nameof(entity), request.Id);
+            }
+
+            _dbContext.Sellers.Remove(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
