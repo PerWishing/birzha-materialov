bba7afa13819091b27bb95091cb6c6c6bb7d02f3
PerWishing <holden201705@gmail.com>
2023-10-19 18:33:11 +0300

    added GetMaterialQuery

---
+++ BirzhaMaterialov.Application/Materials/Queries/GetMaterial/GetMaterialQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using BirzhaMaterialov.Application.Common.Exceptions;
+using BirzhaMaterialov.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Materials.Queries.GetMaterial
+{
+    public class GetMaterialQueryHandler : IRequestHandler<GetMaterialQuery,MaterialVm>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public GetMaterialQueryHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<MaterialVm> Handle(GetMaterialQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Materials.FirstOrDefaultAsync(material => material.Id == request.Id,
+                cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(entity), request.Id);
+            }
+
+            var materialVm = new MaterialVm
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Price = entity.Price
+            };
+
+            return materialVm;
+        }
+    }
+}
