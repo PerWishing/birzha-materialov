3e518825c4947a840a8ea76977b9f7d94d2df102
PerWishing <holden201705@gmail.com>
2023-10-19 18:21:47 +0300

    added DeleteMaterialCommand

---
+++ BirzhaMaterialov.Application/Materials/Commands/DeleteMaterial/DeleteMaterialCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Materials.Commands.DeleteMaterial
+{
+    public class DeleteMaterialCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ BirzhaMaterialov.Application/Materials/Commands/DeleteMaterial/DeleteMaterialCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using BirzhaMaterialov.Application.Common.Exceptions;
+using BirzhaMaterialov.Application.Interfaces;
+using BirzhaMaterialov.Application.Sellers.Commands.DeleteSeller;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Materials.Commands.DeleteMaterial
+{
+    public class DeleteMaterialCommandHandler : IRequestHandler<DeleteMaterialCommand>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public DeleteMaterialCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Handle(DeleteMaterialCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Materials.FirstOrDefaultAsync(material => material.Id == request.Id,
+                cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(entity), request.Id);
+            }
+
+            _dbContext.Materials.Remove(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
