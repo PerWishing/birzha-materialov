e8ce55f55d4596c10f09ec7e0f2b280844216cd2
PerWishing <holden201705@gmail.com>
2023-10-19 18:44:18 +0300

    added GetMaterialsQuery

---
+++ BirzhaMaterialov.Application/Materials/Queries/GetMaterials/GetMaterialsQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using BirzhaMaterialov.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Materials.Queries.GetMaterials
+{
+    public class GetMaterialsQueryHandler : IRequestHandler<GetMaterialsQuery, MaterialsVm>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public GetMaterialsQueryHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<MaterialsVm> Handle(GetMaterialsQuery request, CancellationToken cancellationToken)
+        {
+            var entityList = await _dbContext.Materials.ToListAsync(cancellationToken);
+
+            var materialsVm = new MaterialsVm();
+            materialsVm.Materials = new List<MaterialDto>();
+
+            foreach (var material in entityList)
+            {
+                var materialDto = new MaterialDto
+                {
+                    Id = material.Id,
+                    Name = material.Name,
+                    Price = material.Price
+                };
+
+                materialsVm.Materials.Add(materialDto);
+            }
+
+            return materialsVm;
+        }
+    }
+}
---
+++ BirzhaMaterialov.Application/Materials/Queries/GetMaterials/MaterialsVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Materials.Queries.GetMaterials
+{
+    public class MaterialsVm
+    {
+        public IList<MaterialDto>? Materials { get; set; }
+    }
+}
