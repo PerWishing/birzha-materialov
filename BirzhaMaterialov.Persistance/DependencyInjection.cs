565bdf5a244644fb3628c9791cea6c0f1e5716a7
PerWishing <holden201705@gmail.com>
2023-10-19 20:53:21 +0300

    added DI class to Persistance, use method in Program.cs

---
+++ BirzhaMaterialov.Persistance/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using BirzhaMaterialov.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["PgConnection"];
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+            services.AddScoped<IApplicationDbContext>(provider =>
+            provider.GetService<ApplicationDbContext>()!);
+
+            return services;
+        }
+    }
+}
