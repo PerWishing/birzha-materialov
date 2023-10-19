153eef5c94b76d28144d29087ae522e7aa3c735a
PerWishing <holden201705@gmail.com>
2023-10-19 13:36:07 +0300

    added UpdateSellerCommand

---
+++ BirzhaMaterialov.Application/Sellers/Commands/UpdateSeller/UpdateSellerCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using BirzhaMaterialov.Application.Common.Exceptions;
+using BirzhaMaterialov.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Sellers.Commands.UpdateSeller
+{
+    public class UpdateSellerCommandHandler : IRequestHandler<UpdateSellerCommand>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public UpdateSellerCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Handle(UpdateSellerCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Sellers.FirstOrDefaultAsync(seller => seller.Id == request.Id,
+                cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(entity), request.Id);
+            }
+
+            entity.Name = request.Name;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
