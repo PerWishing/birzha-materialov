795c6bb7d1abd6692ea71cf1cba1605141acdac4
PerWishing <holden201705@gmail.com>
2023-10-20 11:44:22 +0300

    added UpdateMaterialCommandValidator

---
+++ BirzhaMaterialov.Application/Materials/Commands/UpdateMaterial/UpdateMaterialCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Materials.Commands.UpdateMaterial
+{
+    public class UpdateMaterialCommandValidator:AbstractValidator<UpdateMaterialCommand>
+    {
+        public UpdateMaterialCommandValidator()
+        {
+            RuleFor(createMaterialCommand => createMaterialCommand.Name).NotEmpty();
+            RuleFor(createMaterialCommand => createMaterialCommand.Price).GreaterThanOrEqualTo(0);
+        }
+    }
+}
