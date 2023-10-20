f214a63c7e2f1ed9381d565c6fe2da3d845ed1c0
PerWishing <holden201705@gmail.com>
2023-10-20 12:39:31 +0300

    added ApiExceptionFilterAttribute

--- BirzhaMaterialov.WebApi/Controllers/BaseController.cs
+++ BirzhaMaterialov.WebApi/Controllers/BaseController.cs
@@ -1,9 +1,11 @@
-﻿using MediatR;
+﻿using BirzhaMaterialov.WebApi.Filters;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BirzhaMaterialov.WebApi.Controllers
 {
     [ApiController]
+    [ApiExceptionFilter]
     public abstract class BaseController : ControllerBase
     {
         private IMediator? _mediator;
---
+++ BirzhaMaterialov.WebApi/Filters/ApiExceptionFilterAttribute.cs
@@ -0,0 +1,57 @@
+﻿using BirzhaMaterialov.Application.Common.Exceptions;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BirzhaMaterialov.WebApi.Filters
+{
+    public class ApiExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        private readonly IDictionary<Type, Action<ExceptionContext>> _exceptionHandlers;
+
+        public ApiExceptionFilterAttribute()
+        {
+            // Register known exception types and handlers.
+            _exceptionHandlers = new Dictionary<Type, Action<ExceptionContext>>
+            {
+                { typeof(ValidationException), HandleValidationException },
+                { typeof(NotFoundException), HandleNotFoundException },
+            };
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+            HandleException(context);
+
+            base.OnException(context);
+        }
+
+        private void HandleException(ExceptionContext context)
+        {
+            Type type = context.Exception.GetType();
+            if (_exceptionHandlers.ContainsKey(type))
+            {
+                _exceptionHandlers[type].Invoke(context);
+                return;
+            }
+        }
+
+        private void HandleValidationException(ExceptionContext context)
+        {
+            var exception = (ValidationException)context.Exception;
+
+            context.Result = new BadRequestObjectResult(exception.Errors);
+
+            context.ExceptionHandled = true;
+        }
+
+        private void HandleNotFoundException(ExceptionContext context)
+        {
+            var exception = (NotFoundException)context.Exception;
+
+            context.Result = new NotFoundObjectResult(exception.Message);
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
