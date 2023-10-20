024335127c9e858504672e0b4ec2ecfc900aa8da
PerWishing <holden201705@gmail.com>
2023-10-20 15:18:09 +0300

    added Hangfire and HangfireService, incomplite

--- BirzhaMaterialov.WebApi/Program.cs
+++ BirzhaMaterialov.WebApi/Program.cs
@@ -1,6 +1,9 @@
 using BirzhaMaterialov.Application;
 using BirzhaMaterialov.Persistance;
 using BirzhaMaterialov.Persistance.Initializer;
+using BirzhaMaterialov.WebApi.Services;
+using Hangfire;
+using Hangfire.PostgreSql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,19 +12,29 @@ var configuration = builder.Configuration;
 builder.Services.AddApplication();
 builder.Services.AddPersistance(configuration);
 
+var connectionStr = configuration["PgConnection"];
+
+builder.Services.AddHangfire(h =>
+                h.UsePostgreSqlStorage(connectionStr));
+
+builder.Services.AddHangfireServer();
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddTransient<HangfireService>();
 
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHangfireDashboard("/hangfire");
 
 var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
 using (var scope = scopeFactory.CreateScope())
@@ -38,5 +51,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+RecurringJob.AddOrUpdate<HangfireService>(x => x.SetUpdateDbEveryDay(), Cron.Minutely);
 
 app.Run();
---
+++ BirzhaMaterialov.WebApi/Services/HangfireService.cs
@@ -0,0 +1,22 @@
+﻿using BirzhaMaterialov.Application.Materials.Queries.GetMaterials;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirzhaMaterialov.WebApi.Services
+{
+    public class HangfireService
+    {
+        private IMediator? _mediator;
+
+        public HangfireService(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        //test
+        public void SetUpdateDbEveryDay()
+        {
+            var vm = _mediator!.Send(new GetMaterialsQuery()).GetAwaiter().GetResult();
+            Console.WriteLine($"Count: {vm.Materials!.Count}");
+        }
+    }
+}
