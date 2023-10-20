de9275fcd113524322f8ada2320219e029bb7862
PerWishing <holden201705@gmail.com>
2023-10-20 11:39:50 +0300

    added UpdateSellerCommandValidator

---
+++ BirzhaMaterialov.Application/Sellers/Commands/UpdateSeller/UpdateSellerCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Sellers.Commands.UpdateSeller
+{
+    public class UpdateSellerCommandValidator:AbstractValidator<UpdateSellerCommand>
+    {
+        public UpdateSellerCommandValidator()
+        {
+            RuleFor(updateSellerCommand => updateSellerCommand.Name).NotEmpty();
+        }
+    }
+}
