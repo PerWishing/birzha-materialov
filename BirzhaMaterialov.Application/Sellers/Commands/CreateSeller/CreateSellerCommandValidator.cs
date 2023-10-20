bdfdd6dded5c331ecce969d39e45254df4b42b35
PerWishing <holden201705@gmail.com>
2023-10-20 11:35:46 +0300

    added CreateSellerCommandValidator

---
+++ BirzhaMaterialov.Application/Sellers/Commands/CreateSeller/CreateSellerCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirzhaMaterialov.Application.Sellers.Commands.CreateSeller
+{
+    public class CreateSellerCommandValidator : AbstractValidator<CreateSellerCommand>
+    {
+        public CreateSellerCommandValidator()
+        {
+            RuleFor(createSellerCommand => createSellerCommand.Name).NotEmpty();
+        }
+    }
+}
